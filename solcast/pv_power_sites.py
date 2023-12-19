11185b9b49d707417c78d65a7ec678ad770aedee
Hugh Cutcher <hugh@solcast.com.au>
2023-12-19 18:21:24 +1100

    feat: respond to pr comments

--- solcast/pv_power_sites.py
+++ solcast/pv_power_sites.py
@@ -31,7 +31,10 @@ def get_pv_power_site(resource_id: str, **kwargs) -> Response:
 
 
 def create_pv_power_site(
-    name: str, latitude: float, longitude: float, **kwargs
+    name: str,
+    latitude: float,
+    longitude: float,
+    **kwargs,
 ) -> Response:
     """
     Create PV power site to be used with Solcast's advanced PV power model
@@ -40,9 +43,8 @@ def create_pv_power_site(
         name: arbitrary string to identify the location, unique not required
         latitude: in decimal degrees, between -90 and 90, north is positive
         longitude: in decimal degrees, between -180 and 180, east is positive
-        **kwargs:
 
-    See https://docs.solcast.com.au/ for full list of parameters.
+    See https://docs.solcast.com.au/ for more information and full list of parameters.
 
     """
     client = Client(base_url=base_url, endpoint=pv_power_site)
@@ -66,7 +68,6 @@ def patch_pv_power_site(resource_id: str, **kwargs) -> Response:
 
     Args:
         resource_id: unique string to identify the location that is generated when creating a site
-        **kwargs:
 
     See https://docs.solcast.com.au/ for full list of parameters.
 
@@ -90,7 +91,6 @@ def update_pv_power_site(resource_id: str, **kwargs) -> Response:
 
     Args:
         resource_id: unique string to identify the location that is generated when creating a site
-        **kwargs:
 
     See https://docs.solcast.com.au/ for full list of parameters.
 
