34b8ceb4d4fde8aafb6ada81be87bcb7979ee3ef
Lachlan <lachlan@solcast.com.au>
2024-07-04 01:17:50 +0000

    Add historic advanced pv power

--- solcast/historic.py
+++ solcast/historic.py
@@ -1,5 +1,10 @@
 from .api import Client, PandafiableResponse
-from .urls import base_url, historic_radiation_and_weather, historic_rooftop_pv_power
+from .urls import (
+    base_url,
+    historic_radiation_and_weather,
+    historic_rooftop_pv_power,
+    historic_advanced_pv_power,
+)
 
 
 def radiation_and_weather(
@@ -88,6 +93,50 @@ def rooftop_pv_power(
     params = {
         "latitude": latitude,
         "longitude": longitude,
+        "start": start,
+        "format": "json",
+        **kwargs,
+    }
+
+    if end is not None:
+        params["end"] = end
+    if duration is not None:
+        params["duration"] = duration
+
+    return client.get(params)
+
+
+def advanced_pv_power(
+    resource_id: int, start: str, end: str = None, duration: str = None, **kwargs
+) -> PandafiableResponse:
+    """
+    Get historical high spec PV power estimated actuals for the requested site,
+    derived from satellite (clouds and irradiance over non-polar continental areas)
+    and numerical weather models (other data).
+
+    Args:
+        resource_id: a Solcast resource id
+        **kwargs: additional keyword arguments to be passed through as URL parameters to the Solcast API
+        start: datetime-like, first day of the requested period
+        end: optional, datetime-like, last day of the requested period
+        duration: optional, ISO_8601 compliant duration for the historic data.
+            Must be within 31 days of the start_date.
+        **kwargs: additional keyword arguments to be passed through as URL parameters to the Solcast API
+
+    See https://docs.solcast.com.au/ for full list of parameters.
+    """
+    client = Client(
+        base_url=base_url,
+        endpoint=historic_advanced_pv_power,
+        response_type=PandafiableResponse,
+    )
+
+    assert (end is None and duration is not None) | (
+        duration is None and end is not None
+    ), "only one of duration or end"
+
+    params = {
+        "resource_id": resource_id,
         "start": start,
         "format": "json",
         **kwargs,
--- tests/test_historic.py
+++ tests/test_historic.py
@@ -1,7 +1,10 @@
 import pytest
 
 from solcast import historic
-from solcast.unmetered_locations import load_test_locations_coordinates
+from solcast.unmetered_locations import (
+    load_test_locations_coordinates,
+    UNMETERED_LOCATIONS,
+)
 import pandas as pd
 
 
@@ -33,6 +36,18 @@ def test_rooftop_pv_power():
         start="2022-10-25T14:45:00.00Z",
         duration="P3D",
         capacity=10,
+    )
+
+    assert res.success is True
+    assert len(res.to_dict()["estimated_actuals"]) == 3 * 48 + 1
+    assert ~res.to_pandas().isna().any().all()
+
+
+def test_advanced_pv_power():
+    res = historic.advanced_pv_power(
+        resource_id=UNMETERED_LOCATIONS["Sydney Opera House"]["resource_id"],
+        start="2022-10-25T14:45:00.00Z",
+        duration="P3D",
     )
 
     assert res.success is True
